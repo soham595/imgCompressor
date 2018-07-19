2632abdcd815d731f906639074baa937c45bd890
Soham <soham5959595@gmail.com>
2018-07-19 15:09:18 +0530

    Retrieving data from model properly. Added new container for user Input. Added new html to demonstrate model training.

--- smart/views.py
+++ smart/views.py
@@ -9,9 +9,14 @@ def imageUpload(request):
     image.image = request.FILES['image']
     image.iname = request.POST['iname']
     image.save()
-    return render(request, 'smart/index.html')
+    dict = {'image': image}
+    return render(request, 'smart/index.html', dict)
 
 def imageView(request, foo):
     image = Image.objects.get(pk=foo)
     dict = {'image': image}
     return render(request, 'smart/index.html', dict)
+
+def trainView(request):
+    #Insert functionality here
+    return render(request, 'smart/train.html')
